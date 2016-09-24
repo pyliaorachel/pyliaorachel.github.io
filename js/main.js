c79c3e7f35b0613b6933f19515c4b0e4af4e02bd
Rachel P.Y. Liao <u3512485@connect.hku.hk>
2016-09-24 18:06:43 -0400

    Homepage style & timeline layout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+	var path = window.location.pathname.split('/')[1];
+	if (path == '')
+		path = 'home';
+	const activeNavElementStr = '.nav-' + path;
+	$(activeNavElementStr).addClass('active');
+
 	// header
 	$('#nav-links>li').click(function(){
 		$('#nav-links>li.active').removeClass('active');
