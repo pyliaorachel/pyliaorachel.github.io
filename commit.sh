235affc99927f973fef28b3db791aa5130c0d48f
Rachel P.Y. Liao <u3512485@connect.hku.hk>
2016-10-12 17:42:20 -0400

    1 post

---
+++ commit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ $# -ne 1 ]
+then
+	echo "Should provide your commit message!"
+else
+	msg=$1
+
+	echo "${msg}"
+	jekyll build
+	git add -A
+	git commit -m "${msg}"
+	git push origin master:source
+	cd _site
+	touch .nojekyll
+	git add -A
+	git commit -m "${msg}"
+	git push origin master
+	cd ..
+fi
